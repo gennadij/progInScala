618bb19a6510a03ed6961f2900d4186b66e03271
Gennadi Heimann <gennadiheimann@gmail.com>
2016-02-22 23:57:21 +0100

    chapter 7 -> for expression

--- progInScala/src/chapter7/ForExpression.scala
+++ progInScala/src/chapter7/ForExpression.scala
@@ -1,11 +1,30 @@
 package chapter7
 
 class ForExpression {
-  def callFor = {
-    
-    val fileHere = (new java.io.File("..").listFiles)
   
-    for (file <- fileHere)
+  val filesHere = (new java.io.File("./src").listFiles)
+  
+  def callFor = {
+    for (file <- filesHere)
       println(file)
   }
+  
+  def fileLines(file: java.io.File) = 
+    scala.io.Source.fromFile(file).getLines().toList
+  
+  def grep(pattern:String) = {
+    for(
+      file <- filesHere
+      if file.getName.endsWith(".scala");
+      line <- fileLines(file)
+      if line.trim.matches(pattern)
+    )
+    println(file + ": " + line.trim)
+  }
+  
+  def scalaFiles = 
+    for {
+      file <- filesHere
+      if file.getName.endsWith(".scala")
+    } yield file
 }
--- progInScala/src/main/Main.scala
+++ progInScala/src/main/Main.scala
@@ -5,14 +5,12 @@ import chapter4.ChecksumAccumulator
 import chapter7.ForExpression
 
 object Main {
-  
+  println("*****************chapter 6 *************")
   def main(args: Array[String]) = {
     val a = new Rational(4, 5)
     val b = new Rational(7, 8)
     val c = new Rational(8, 9)
     val d = new Rational(11, 10)
-    
-    
     
     println(a + b)
     println(a * b)
@@ -23,9 +21,13 @@ object Main {
     
     println(checkSum)
     
+    println("****************chapter 7***************")
     
     val forExp = new ForExpression
     
-    forExp.callFor
+    forExp.grep(".*Name.*")
+    println("**************************")
+    val arrayOfFiles = forExp.scalaFiles
+    for(file <- arrayOfFiles) println(file)
   }
 }
