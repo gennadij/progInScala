0b7c56e4dc961edd3cab5da4d331b36cbcca8a1c
Gennadi Heimann <genaheimann@gmail.com>
2016-03-13 23:39:48 +0100

    add old projects

---
+++ progInScala/src/scalatrain/Train.scala
@@ -0,0 +1,25 @@
+/**
+  * Created by gennadi on 17.01.16.
+  */
+case class Train(info : TrainInfo,schedule: Seq[(Time, Station)]) {
+  
+  require(info != null, "info must not be null")
+  require(schedule != null, "schedule must not be null")
+  require(schedule.size >= 2, "schedule must have at least two stops")
+  
+  val stations: Seq[Station] = schedule map { _._2 }
+}
+
+case class Station(name: String) {
+  require(name != null, "name must not be null")
+}
+
+sealed abstract class TrainInfo {
+  def number:String
+}
+
+case class Ice(number: String, hasWifi: Boolean = false) extends TrainInfo
+
+case class Re(number: String) extends TrainInfo
+
+case class Brb(number: String) extends TrainInfo
